a583c71ff6957214d20e24ac74ac8ff3eecbc724
Sylvia Wang <sylviaaa0412@gmail.com>
2024-08-28 20:39:24 +0800

    Homework4 finish

--- src/hw4/Hw405.java
+++ src/hw4/Hw405.java
@@ -12,12 +12,48 @@ import java.util.Scanner;
 (提示3:擋下錯誤輸入:例如月份輸入為2,則日期不該超過29)
 */
 public class Hw405 {
-public static void main(String[]args) {
-	
-	//1.鍵盤輸入三個整數
-	Scanner sc = new Scanner(System.in);
-	
-	
-	
-}
+	public static void main(String[] args) {
+
+		// 1.鍵盤輸入年、月、日
+		Scanner sc = new Scanner(System.in);
+		System.out.println("請輸入年份(YYYY)");
+		int year = sc.nextInt();
+		System.out.println("請輸入月份(MM)");
+		int month = sc.nextInt();
+		System.out.println("請輸入日期(DD)");
+		int day = sc.nextInt();
+
+		// 2.建立日期陣列
+		int[] dayOfMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+		// 3.判斷閏年
+		boolean leapYear = ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+		if (leapYear) {
+			dayOfMonth[1] = 29;
+		}
+
+		// 3.判斷輸入的月份及日期是否正確
+		if (year < 1) {
+			System.out.println("請輸入正確年份");
+			return;
+		} else if (month < 1 || month > 12) {
+			System.out.println("請輸入正確月份");
+			return;
+		}
+		for (int i = 0; i < dayOfMonth.length; i++) {
+			if ((month == i + 1) && day > dayOfMonth[i]) {
+				System.out.println("請輸入正確日期");
+				return;
+			}
+		}
+
+		// 4.計算是一年中第幾天
+		int count = 0;
+		for (int i = 0; i < month - 1; i++) {
+			count += dayOfMonth[i];
+		}
+		count += day;
+		System.out.println(year + "/" + month + "/" + day + "是一年中的第" + count + "天");
+
+	}
 }
--- src/hw4/Hw406.java
+++ src/hw4/Hw406.java
@@ -1,10 +1,35 @@
 package hw4;
+
 /*
 班上有8位同學,他們進行了6次考試結果如下:
 請算出每位同學考最高分的次數
 */
 public class Hw406 {
-public static void main(String[]args) {
-	
-}
+	public static void main(String[] args) {
+
+		// 1.建立二維陣列儲存成績
+		int[][] score = { { 10, 35, 40, 100, 90, 85, 75, 70 }, { 37, 75, 77, 89, 64, 75, 70, 95 },
+				{ 100, 70, 79, 90, 75, 70, 79, 90 }, { 77, 95, 70, 89, 60, 75, 85, 89 },
+				{ 98, 70, 89, 90, 75, 90, 89, 90 }, { 90, 80, 100, 75, 50, 20, 99, 75 } };
+
+		// 2.建立陣列紀錄每位學生最高分次數
+		int[] topScore = new int[score[0].length];
+
+		// 3.循環找出每次考試最高分
+
+		for (int i = 0; i < score.length; i++) {
+			int index = 0, max = score[i][0];
+			for (int j = 0; j < score[i].length; j++) {
+				if (score[i][j] > max) {
+					max = score[i][j];
+					index = j;
+				}
+
+			}
+			topScore[index]++;
+		}
+		for (int i = 0; i < topScore.length; i++) {
+			System.out.println((i + 1) + "號同學考了" + topScore[i] + "次最高分");
+		}
+	}
 }
